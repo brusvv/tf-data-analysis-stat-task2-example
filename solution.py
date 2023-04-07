aadc2f7feb010c6a5fbcd71f51469ccfb1323ea2
Vladimir <volodyabrus@gmail.com>
2023-04-08 00:32:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,13 +6,13 @@ from scipy.stats import norm
 
 chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
-  t = 56
-  n = len(x)
-  mu = 0
-  sigma = 1
-
-  x_error = np.random.normal(mu, sigma, n)
-  a = 2 * x_error/ t**2
-
-  return a.mean()
+def solution(p: float, x: np.array) -> tuple:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    n = len(x)
+    a = 0.005
+    alpha = 1 - p
+    lower_bound = uniform.ppf(alpha / 2, loc=a, scale=(np.max(x) - a) / n)
+    upper_bound = uniform.ppf(1 - alpha / 2, loc=a, scale=(np.max(x) - a) / n)
+    return (lower_bound, upper_bound)
