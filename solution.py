5d17c1f059450afea257f9a5a6a94cee1499a980
Vladimir <volodyabrus@gmail.com>
2023-04-08 00:54:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
-chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
+chat_id = 5437824033  # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)
-    a = 0.005
+    
     alpha = 1 - p
-    lower_bound = uniform.ppf(alpha / 2, loc=a, scale=(np.max(x) - a) / n)
-    upper_bound = uniform.ppf(1 - alpha / 2, loc=a, scale=(np.max(x) - a) / n)
-    return (lower_bound, upper_bound)
+    loc = (2 * x / 56**2).mean()
+    scale = np.sqrt(np.var(2 * x / 56**2)) / np.sqrt(len(x))
+    return 2 * len(x) * loc / chi2.ppf(alpha / 2, 2*len(x)), \
+           2 * len(x) * loc / chi2.ppf(1 - alpha / 2, 2*len(x))
