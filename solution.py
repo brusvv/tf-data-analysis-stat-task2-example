1a1dd6a065971c3033adde2583a2a94aed458694
Vladimir <volodyabrus@gmail.com>
2023-04-08 03:55:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,11 +3,17 @@ import numpy as np
 
 from scipy.stats import norm
 from scipy.stats import chi2
+from scipy.stats import uniform
 
 
 chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    length = len(x)
-    return np.sqrt(sum(x ** 2) / (56 * chi2.ppf((1 + p) / 2, df = 2 * length))), \
-           np.sqrt(sum(x ** 2) / (56 * chi2.ppf((1 - p) / 2, df = 2 * length)))
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 1 - p
+    loc = x.mean()
+    scale = loc / pow(p, len(x))
+    return loc * uniform.ppf(1 - alpha / 2), \
+           scale * uniform.ppf(alpha / 2)
